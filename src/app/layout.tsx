574be3452a7c29c5fb10f09cbaf86e02a3dda67c
VictorHOgawa <victor.ogawa@gmail.com>
2024-09-19 08:47:23 -0400

    add clarity gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "swiper/css";
 import "react-day-picker/style.css";
 import { CookiesProvider } from "next-client-cookies/server";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script type="text/javascript" id="clarity">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "o5nqnjy3jk");`}
+      </Script>
+      <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WJS4FLP');`}
+      </Script>
       <CookiesProvider>
         <body className={inter.className}>{children}</body>
       </CookiesProvider>
