cc1a1b915dddf0d50b48451e9ecf92f58e3259b7
VictorHOgawa <victor.ogawa@gmail.com>
2025-04-04 14:23:14 -0300

    feat(feature): new functions

---
+++ src/utils/utils.ts
@@ -0,0 +1,12 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function shortenNumber(number: number) {
+  if (number < 1000) return number.toString();
+  if (number < 1000000) return `${(number / 1000).toFixed(1)}k`;
+  return `${(number / 1000000).toFixed(1)}M`;
+}
