50cb33d872cbcfba7411926d75b41030c392f538
VictorHOgawa <victor.ogawa@gmail.com>
2025-04-04 14:35:48 -0300

    refactor(refactor): swap to new word cloud lib

--- src/components/app/home/WordCloud.tsx
+++ src/components/app/home/WordCloud.tsx
@@ -1,11 +1,15 @@
 "use client";
-import ReactWordcloud from "react-wordcloud";
-import { useEffect, useState } from "react";
 import { BaseCard } from "@/components/global/BaseCard/BaseCard";
-import { BaseCardHeader } from "@/components/global/BaseCard/BaseCardHeader";
 import { BaseCardFooter } from "@/components/global/BaseCard/BaseCardFooter";
-import { useSocialMediaDataContext } from "@/context/SocialMediaData";
+import { BaseCardHeader } from "@/components/global/BaseCard/BaseCardHeader";
 import { Skeleton } from "@/components/global/Skeleton";
+import { useSocialMediaDataContext } from "@/context/SocialMediaData";
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
+import { scaleLog } from "@visx/scale";
+import { Text } from "@visx/text";
+import Wordcloud from "@visx/wordcloud/lib/Wordcloud";
+import { useEffect, useMemo, useState } from "react";
+import { WordData } from "../comparator/ActiveWordCloud";
 
 interface WordCloudProps {
   WordCloudData: {
@@ -29,11 +33,11 @@ interface WordsProps {
 }
 
 export function WordCloud({ WordCloudData }: WordCloudProps) {
-  const [facebookWords, setFacebookWords] = useState<WordsProps[]>();
-  const [instagramWords, setInstagramWords] = useState<WordsProps[]>();
-  const [tiktokWords, setTiktokWords] = useState<WordsProps[]>();
-  const [youtubeWords, setYoutubeWords] = useState<WordsProps[]>();
-  const [wordsList, setWordsList] = useState<WordsProps[]>();
+  const [facebookWords, setFacebookWords] = useState<WordsProps[]>([]);
+  const [instagramWords, setInstagramWords] = useState<WordsProps[]>([]);
+  const [tiktokWords, setTiktokWords] = useState<WordsProps[]>([]);
+  const [youtubeWords, setYoutubeWords] = useState<WordsProps[]>([]);
+  const [wordsList, setWordsList] = useState<WordsProps[]>([]);
   const { isGettingData, socialMediaData } = useSocialMediaDataContext();
 
   useEffect(() => {
@@ -78,18 +82,96 @@ export function WordCloud({ WordCloudData }: WordCloudProps) {
     setWordsList(flatWordsList);
   }, [facebookWords, instagramWords, tiktokWords, youtubeWords]);
 
+  const colors = ["#143059", "#2F6B9A", "#82a6c2"];
+
+  function wordFreq(text: string): WordData[] {
+    const words: string[] = text.replace(/\./g, "").split(/\s/);
+    const freqMap: Record<string, number> = {};
+
+    for (const w of words) {
+      if (!freqMap[w]) freqMap[w] = 0;
+      freqMap[w] += 1;
+    }
+    return Object.keys(freqMap).map((word) => ({
+      text: word,
+      value: freqMap[word],
+    }));
+  }
+
+  function getRotationDegree() {
+    const rand = Math.random();
+    const degree = rand > 0.5 ? 60 : -60;
+    return rand * degree;
+  }
+
+  const fontScale = scaleLog({
+    domain: [
+      Math.min(...wordsList.map((w) => w.value)),
+      Math.max(...wordsList.map((w) => w.value)),
+    ],
+    range: [10, 100],
+  });
+  const fontSizeSetter = (datum: WordData) => fontScale(datum.value);
+
+  const fixedValueGenerator = () => 0.5;
+
+  type SpiralType = "archimedean" | "rectangular";
+  const [spiralType, setSpiralType] = useState<SpiralType>("archimedean");
+  const [withRotation, setWithRotation] = useState(false);
+
+  const sortedWords = useMemo(
+    () => wordsList.sort((a, b) => b.value - a.value).slice(0, 200),
+    [wordsList],
+  );
+
   return (
     <BaseCard className="p-0">
       <BaseCardHeader title="Nuvem de Palavras" />
       {isGettingData ? (
         <Skeleton className="mx-auto mt-4 h-[17rem] w-11/12" />
       ) : (
-        <div className="flex h-48 w-full flex-col lg:mb-0 lg:h-[calc(100%-5.5rem)]">
-          <ReactWordcloud
-            words={wordsList as WordsProps[]}
-            options={WordCloudData.options}
-          />
-        </div>
+        <>
+          {typeof window !== "undefined" &&
+            sortedWords &&
+            sortedWords.length !== 0 && (
+              <ParentSize>
+                {({ width, height }) => (
+                  <Wordcloud
+                    words={sortedWords}
+                    width={width}
+                    height={height}
+                    fontSize={fontSizeSetter}
+                    font={"Impact"}
+                    padding={2}
+                    spiral={spiralType}
+                    rotate={withRotation ? getRotationDegree : 0}
+                    random={fixedValueGenerator}
+                  >
+                    {(cloudWords) =>
+                      cloudWords.map((w, i) => (
+                        <Text
+                          key={`${i}` + w.text}
+                          fill={colors[i % colors.length]}
+                          textAnchor={"middle"}
+                          transform={`translate(${w.x}, ${w.y}) rotate(${w.rotate})`}
+                          fontSize={w.size}
+                          fontFamily={w.font}
+                        >
+                          {w.text}
+                        </Text>
+                      ))
+                    }
+                  </Wordcloud>
+                )}
+              </ParentSize>
+            )}
+        </>
+        // <div className="flex h-48 w-full flex-col lg:mb-0 lg:h-[calc(100%-5.5rem)]">
+        //   <ReactWordcloud
+        //     words={wordsList as WordsProps[]}
+        //     options={WordCloudData.options}
+        //   />
+        // </div>
       )}
       <BaseCardFooter text="Nuvem das palavras mais utilizadas." />
     </BaseCard>
